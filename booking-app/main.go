c034d19fb853939ac1843177d4d5a2fda1aa35c8
Jose Kupeka Muhlanga <muhlangakupeka@outlook.com>
2026-01-26 20:47:35 +0300

    Draft completion of notes

--- booking-app/main.go
+++ booking-app/main.go
@@ -14,11 +14,13 @@ func main(){
 	fmt.Printf("We have a total of %v Tickets and %v are still available\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")	
 	
+	var bookings = []string{}
+	
 	var firstName string
 	var lastName string
 	var email string
 	var userTickets int
-	
+		
 	// ask user for name
 	fmt.Println("Enter your first name")
 	fmt.Scan(&firstName)
@@ -33,6 +35,14 @@ func main(){
 	fmt.Scan(&userTickets)
 	
 	remainingTickets = remainingTickets - uint(userTickets)
+
+	bookings = append(bookings, firstName +" "+lastName)
+	
+	fmt.Printf("The Whole slice is %v\n", bookings)
+	fmt.Printf("The first element is %v\n", bookings[0])
+	fmt.Printf("The slice type is %T\n", bookings)
+	fmt.Printf("The slice length is %v\n", len(bookings))
 	
 	fmt.Printf("User %v %v booked %v tickets you will get an email at %v\n",firstName,lastName, userTickets,email)
+	fmt.Printf("%v tickets remaining for %v\n",remainingTickets,conferenceName)
 }
